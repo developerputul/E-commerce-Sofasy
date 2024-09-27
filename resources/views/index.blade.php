3a1d514c6294bd69c575b273a348318af19e9465
developerputul <putulsentongchangya7@gmail.com>
2024-09-27 20:22:43 +0600

    Frontend User Dashboard Password Change and Login Section Updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('dashboard')
 @section('user')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <div class="page-header breadcrumb-wrap">
         <div class="container">
@@ -44,7 +44,12 @@
                                             aria-selected="true"><i class="fi-rs-user mr-10"></i>Account details</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="page-login.html"><i
+                                        <a class="nav-link" id="change-password-tab" data-bs-toggle="tab"
+                                            href="#change-password" role="tab" aria-controls="change-password"
+                                            aria-selected="true"><i class="fi-rs-user mr-10"></i>Change Password</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('user.logout') }}"><i
                                                 class="fi-rs-sign-out mr-10"></i>Logout</a>
                                     </li>
                                 </ul>
@@ -58,7 +63,9 @@
                                         <div class="card-header">
                                             <h3 class="mb-0">Hello {{ Auth::user()->name }}!</h3>
                                             <br>
-                                            <img id="showImage" src="{{ (!empty($userData->photo)) ? url('upload/user_images/'.$userData->photo) : url('upload/no_image.jpg ') }}" alt="User" class="rounded-circle p-1 bg-primary" width="110">
+                                            <img id="showImage"
+                                                src="{{ !empty($userData->photo) ? url('upload/user_images/' . $userData->photo) : url('upload/no_image.jpg ') }}"
+                                                alt="User" class="rounded-circle p-1 bg-primary" width="110">
                                         </div>
                                         <div class="card-body">
                                             <p>
@@ -184,64 +191,133 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="tab-pane fade" id="account-detail" role="tabpanel"
-                                    aria-labelledby="account-detail-tab">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h5>Account Details</h5>
-                                        </div>
-                                        <div class="card-body">
-                                            
-                            
-                        <form method="POST" action="{{ route('user.profile.store') }}" enctype="multipart/form-data">
+
+            <div class="tab-pane fade" id="account-detail" role="tabpanel"
+                aria-labelledby="account-detail-tab">
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Account Details</h5>
+                    </div>
+                    <div class="card-body">
+
+
+                        <form method="POST" action="{{ route('user.profile.store') }}"
+                            enctype="multipart/form-data">
                             @csrf
 
-                                <div class="row">
-                                    <div class="form-group col-md-6">
-                                        <label>User Name <span class="required">*</span></label>
-                                        <input required="" class="form-control" name="username"
-                                            type="text" value="{{ $userData->username }}"/>
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label>Full Name <span class="required">*</span></label>
-                                        <input required="" class="form-control" name="name"  value="{{ $userData->name }}"/>
-                                    </div>
-                                    <div class="form-group col-md-12">
-                                        <label>Email<span class="required">*</span></label>
-                                        <input required="" class="form-control" name="email"
-                                            type="text" value="{{ $userData->email }}" />
-                                    </div>
-                                    <div class="form-group col-md-12">
-                                        <label>Phone <span class="required">*</span></label>
-                                        <input required="" class="form-control" name="phone"
-                                            type="text" value="{{ $userData->phone }}"/>
-                                    </div>
-                                    <div class="form-group col-md-12">
-                                        <label>Address<span class="required">*</span></label>
-                                        <input required="" class="form-control" name="address"
-                                            type="text" value="{{ $userData->address }}"/>
-                                    </div>
-                                    <div class="form-group col-md-12">
-                                        <label>User Photo <span class="required">*</span></label>
-                                        <input class="form-control" name="photo"
-                                            type="file" id="image"/>
-                                    </div>
-                                  
-                                    <div class="form-group col-md-12">
-                                        <label> <span class="required">*</span></label>
-                                        <img id="showImage" src="{{ (!empty($userData->photo)) ? url('upload/user_images/'.$userData->photo) : url('upload/no_image.jpg ') }}" alt="User" class="rounded-circle p-1 bg-primary" width="110">
-                                    </div>
-                                  
-                                    <div class="col-md-12">
-                                        <button type="submit"
-                                            class="btn btn-fill-out submit font-weight-bold"
-                                            name="submit" value="Submit">Save Change</button>
-                                    </div>
+                            <div class="row">
+                                <div class="form-group col-md-6">
+                                    <label>User Name <span class="required">*</span></label>
+                                    <input required="" class="form-control" name="username"
+                                        type="text" value="{{ $userData->username }}" />
                                 </div>
-                            </form>
-                                        </div>
-                                    </div>
+                                <div class="form-group col-md-6">
+                                    <label>Full Name <span class="required">*</span></label>
+                                    <input required="" class="form-control" name="name"
+                                        value="{{ $userData->name }}" />
                                 </div>
+                                <div class="form-group col-md-12">
+                                    <label>Email<span class="required">*</span></label>
+                                    <input required="" class="form-control" name="email"
+                                        type="text" value="{{ $userData->email }}" />
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label>Phone <span class="required">*</span></label>
+                                    <input required="" class="form-control" name="phone"
+                                        type="text" value="{{ $userData->phone }}" />
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label>Address<span class="required">*</span></label>
+                                    <input required="" class="form-control" name="address"
+                                        type="text" value="{{ $userData->address }}" />
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label>User Photo <span class="required">*</span></label>
+                                    <input class="form-control" name="photo" type="file"
+                                        id="image" />
+                                </div>
+
+                                <div class="form-group col-md-12">
+                                    <label> <span class="required">*</span></label>
+                                    <img id="showImage"
+                                        src="{{ !empty($userData->photo) ? url('upload/user_images/' . $userData->photo) : url('upload/no_image.jpg ') }}"
+                                        alt="User" class="rounded-circle p-1 bg-primary"
+                                        width="110">
+                                </div>
+
+                                <div class="col-md-12">
+                                    <button type="submit"
+                                        class="btn btn-fill-out submit font-weight-bold"
+                                        name="submit" value="Submit">Save Change</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            {{-- //Change Password --}}
+            <div class="tab-pane fade" id="change-password" role="tabpanel"
+                aria-labelledby="change-password-tab">
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Change Password</h5>
+                    </div>
+                    <div class="card-body">
+
+
+                        <form method="POST" action="{{ route('user.update.password') }}">
+                            @csrf
+
+                            @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+
+                            @elseif (session('error'))
+                            <div class="alert alert-danger" role="alert">
+                            </div>
+                            @endif
+
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label>Old password<span class="required">*</span></label>
+                                     <input class="form-control @error('old_password') is-invalid @enderror" name="old_password"
+                                    type="password" id="current_password" placeholder="Old Password" />
+
+                                    @error('old_password')
+                                    <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group col-md-12">
+                                    <label>New password<span class="required">*</span></label>
+                                     <input class="form-control @error('new_password') is-invalid @enderror" name="new_password"
+                                    type="password" id="new_password" placeholder="New Password" />
+
+                                    @error('new_password')
+                                    <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group col-md-12">
+                                    <label>Confirm New password<span class="required">*</span></label>
+                                     <input class="form-control" name="new_password_confirmation"
+                                    type="password" id="new_password_confirmation" placeholder="Confirm New Password" />
+                                </div>
+
+
+                                <div class="col-md-12">
+                                    <button type="submit"
+                                        class="btn btn-fill-out submit font-weight-bold"
+                                        name="submit" value="Submit">Save Change</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
                             </div>
                         </div>
                     </div>
@@ -251,16 +327,15 @@
     </div>
 
     <script type="text/javascript">
-        $(document).ready(function(){
-    
-            $('#image').change(function(e){
+        $(document).ready(function() {
+
+            $('#image').change(function(e) {
                 var reader = new FileReader();
-                reader.onload = function(e){
-                    $('#showImage').attr('src',e.target.result);
+                reader.onload = function(e) {
+                    $('#showImage').attr('src', e.target.result);
                 }
                 reader.readAsDataURL(e.target.files['0']);
             });
         });
     </script>
-
 @endsection
