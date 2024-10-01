d1d67e246aa54c11a0c0dde27fdfb1999ad56d49
developerputul <putulsentongchangya7@gmail.com>
2024-10-01 21:38:31 +0600

    Admin Dashboard SubCategory All Section Updated

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\SubCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -72,9 +73,7 @@ Route::middleware(['auth','role:vendor'])->group(function(){
 Route::get('/admin/login',[AdminController::class, 'AdminLogin']);
 Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
 
-
 Route::middleware(['auth','role:admin'])->group(function(){
-
 //All Brand Route
 Route::controller(BrandController::class)->group(function(){
     Route::get('/all/brand', 'AllBrand')->name('all.brand');
@@ -83,19 +82,26 @@ Route::controller(BrandController::class)->group(function(){
     Route::get('/edit/brand/{id}', 'EditBrand')->name('edit.brand');
     Route::post('/update/brand', 'UpdateBrand')->name('update.brand');
     Route::get('/delete/brand{id}', 'DeleteBrand')->name('delete.brand');
-
 });
-
 //All Category Route
 Route::controller(CategoryController::class)->group(function(){
     Route::get('/all/category', 'AllCategory')->name('all.category');
     Route::get('/add/category', 'AddCategory')->name('add.category');
     Route::post('/category/store', 'CategoryStore')->name('category.store');
     Route::get('/edit/category/{id}', 'EditCategory')->name('edit.category');
-
     Route::post('/update/category', 'UpdateCategory')->name('update.category');
     Route::get('/delete/category{id}', 'DeleteCategory')->name('delete.category');
+});
 
+//All SubCategory Route
+Route::controller(SubCategoryController::class)->group(function(){
+    Route::get('/all/subcategory', 'AllSubCategory')->name('all.subcategory');
+    Route::get('/add/subcategory', 'AddSubCategory')->name('add.subcategory');
+    Route::post('/subcategory/store', 'SubCategoryStore')->name('subcategory.store');
+
+    Route::get('/edit/subcategory/{id}', 'EditSubCategory')->name('edit.subcategory');
+    Route::post('/update/subcategory', 'UpdateSubCategory')->name('update.subcategory');
+    Route::get('/delete/subcategory{id}', 'DeleteSubCategory')->name('delete.subcategory');
 });
 
 
