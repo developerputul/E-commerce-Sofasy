1fa6894d86d843b2cfce9bea59d39ddb2614cb84
developerputul <putulsentongchangya7@gmail.com>
2024-11-15 20:34:01 +0600

    Frontend All Cart Wishlist or Compare Details Section Updated

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -17,6 +17,7 @@
 @include('frontend.home.home_feature_product')
 <!--End Best Sales-->
 
+
 <!-- Flowers Category -->
 <section class="product-tabs section-padding position-relative">
     <div class="container">
@@ -39,14 +40,18 @@
                                         style="height: 120px; width: 120px;" />
                                     </a>
                                 </div>
-                                <div class="product-action-1">
-                                    <a aria-label="Add To Wishlist" class="action-btn" href="shop-wishlist.html"><i
-                                            class="fi-rs-heart"></i></a>
-                                    <a aria-label="Compare" class="action-btn" href="shop-compare.html"><i
-                                            class="fi-rs-shuffle"></i></a>
 
+                                <div class="product-action-1">
+                                    <a aria-label="Add To Wishlist" class="action-btn"
+                                    id="{{ $product->id }}" onclick="addToWishList(this.id)"><i
+                                    class="fi-rs-heart"></i></a>
+    
+                                    <a aria-label="Compare" class="action-btn" id="{{ $product->id }}"
+                                    onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
+    
                                     <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal"
-                                    data-bs-target="#quickViewModal" id="{{ $product->id }}" onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
+                                    data-bs-target="#quickViewModal" id="{{ $product->id }}"
+                                    onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
                                 </div>
 
                                 @php
@@ -137,14 +142,18 @@
                                         style="height: 120px; width: 120px;" />
                                     </a>
                                 </div>
+
                                 <div class="product-action-1">
-                                    <a aria-label="Add To Wishlist" class="action-btn" href="shop-wishlist.html"><i
-                                            class="fi-rs-heart"></i></a>
-                                    <a aria-label="Compare" class="action-btn" href="shop-compare.html"><i
-                                            class="fi-rs-shuffle"></i></a>
+                                    <a aria-label="Add To Wishlist" class="action-btn"
+                                    id="{{ $product->id }}" onclick="addToWishList(this.id)"><i
+                                    class="fi-rs-heart"></i></a>
+
+                                    <a aria-label="Compare" class="action-btn" id="{{ $product->id }}"
+                                    onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
 
                                     <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal"
-                                    data-bs-target="#quickViewModal" id="{{ $product->id }}" onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
+                                    data-bs-target="#quickViewModal" id="{{ $product->id }}"
+                                    onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
                                 </div>
 
                                 @php
@@ -235,13 +244,16 @@
                                     </a>
                                 </div>
                                 <div class="product-action-1">
-                                    <a aria-label="Add To Wishlist" class="action-btn" href="shop-wishlist.html"><i
-                                            class="fi-rs-heart"></i></a>
-                                    <a aria-label="Compare" class="action-btn" href="shop-compare.html"><i
-                                            class="fi-rs-shuffle"></i></a>
+                                <a aria-label="Add To Wishlist" class="action-btn"
+                                id="{{ $product->id }}" onclick="addToWishList(this.id)"><i
+                                class="fi-rs-heart"></i></a>
 
-                                    <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal"
-                                        data-bs-target="#quickViewModal" id="{{ $product->id }}" onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
+                                <a aria-label="Compare" class="action-btn" id="{{ $product->id }}"
+                                onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
+
+                                <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal"
+                                data-bs-target="#quickViewModal" id="{{ $product->id }}"
+                                onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
                                 </div>
 
                                 @php
@@ -331,14 +343,18 @@
                                         style="height: 120px; width: 120px;" />
                                     </a>
                                 </div>
-                                <div class="product-action-1">
-                                    <a aria-label="Add To Wishlist" class="action-btn" href="shop-wishlist.html"><i
-                                            class="fi-rs-heart"></i></a>
-                                    <a aria-label="Compare" class="action-btn" href="shop-compare.html"><i
-                                            class="fi-rs-shuffle"></i></a>
 
+                                <div class="product-action-1">
+                                    <a aria-label="Add To Wishlist" class="action-btn"
+                                    id="{{ $product->id }}" onclick="addToWishList(this.id)"><i
+                                    class="fi-rs-heart"></i></a>
+    
+                                    <a aria-label="Compare" class="action-btn" id="{{ $product->id }}"
+                                    onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
+    
                                     <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal"
-                                        data-bs-target="#quickViewModal" id="{{ $product->id }}" onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
+                                    data-bs-target="#quickViewModal" id="{{ $product->id }}"
+                                    onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
                                 </div>
 
                                 @php
--- resources/views/frontend/vendor/vendor_details.blade.php
+++ resources/views/frontend/vendor/vendor_details.blade.php
@@ -74,14 +74,20 @@
                                                 style="height: 120px; width: 120px;" />
                                         </a>
                                     </div>
+
                                     <div class="product-action-1">
-                                        <a aria-label="Add To Wishlist" class="action-btn" href="shop-wishlist.html"><i
-                                                class="fi-rs-heart"></i></a>
-                                        <a aria-label="Compare" class="action-btn" href="shop-compare.html"><i
-                                                class="fi-rs-shuffle"></i></a>
+                                        <a aria-label="Add To Wishlist" class="action-btn"
+                                        id="{{ $product->id }}" onclick="addToWishList(this.id)"><i
+                                        class="fi-rs-heart"></i></a>
+        
+                                        <a aria-label="Compare" class="action-btn" id="{{ $product->id }}"
+                                        onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
+        
                                         <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal"
-                                            data-bs-target="#quickViewModal"><i class="fi-rs-eye"></i></a>
+                                        data-bs-target="#quickViewModal" id="{{ $product->id }}"
+                                        onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
                                     </div>
+
 
                                     @php
                                         $amount = $product->selling_price - $product->discount_price;
@@ -102,7 +108,8 @@
                                     </div>
 
                                 <h2><a
-                                href="{{ url('product/details/'.$product->id .'/'.$product->product_slug) }}">{{ $product->product_name }}</a>
+                                href="{{ url('product/details/'.$product->id .'/'.$product->product_slug) }}">
+                                {{ $product->product_name }}</a>
                                 </h2>
 
                                     <div class="product-rate-cover">
@@ -137,8 +144,8 @@
                                         @endif
 
                                         <div class="add-cart">
-                                            <a class="add" href="shop-cart.html">
-                                                <i class="fi-rs-shopping-cart mr-5"></i>Add </a>
+                                            <a class="add" href="{{ url('product/details/'.$product->id .'/'.$product->product_slug) }}">
+                                                <i class="fi-rs-shopping-cart mr-5"></i>Details </a>
                                         </div>
                                     </div>
                                 </div>
