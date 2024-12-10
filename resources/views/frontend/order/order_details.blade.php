1bcd37d1256691dea97fb577b86e97a3a1b863bf
developerputul <putulsentongchangya7@gmail.com>
2024-12-10 15:07:38 +0600

    Users Order In Profile Section Updated

--- resources/views/frontend/order/order_details.blade.php
+++ resources/views/frontend/order/order_details.blade.php
@@ -122,16 +122,106 @@
         </div>
     </div>
 
-    <script type="text/javascript">
-        $(document).ready(function() {
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <table class="table" style="font-weight:600;">
+                        <tbody>
+                            <tr>
+                                <td class="col-md-1">
+                                    <label>Image</label>
+                                </td>
 
-            $('#image').change(function(e) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    $('#showImage').attr('src', e.target.result);
-                }
-                reader.readAsDataURL(e.target.files['0']);
-            });
-        });
-    </script>
+                                <td class="col-md-2">
+                                    <label>Product Name</label>
+                                </td>
+
+                                <td class="col-md-2">
+                                    <label>Vendor Name</label>
+                                </td>
+
+                                <td class="col-md-2">
+                                    <label>Product Code</label>
+                                </td>
+
+                                <td class="col-md-1">
+                                    <label>Color</label>
+                                </td>
+
+                                <td class="col-md-1">
+                                    <label>Size</label>
+                                </td>
+
+                                <td class="col-md-1">
+                                    <label>Quantity</label>
+                                </td>
+                                
+                                <td class="col-md-3">
+                                    <label>Price</label>
+                                </td>
+                            </tr>
+
+
+                            @foreach ($orderItem as $item)
+                            <tr>
+                                <td class="col-md-1">
+                                    <label><img src="{{ asset($item->product->product_thambnail) }}"
+                                         style="width:50px; height:50px;"></label>
+                                </td>
+
+                                <td class="col-md-2">
+                                    <label>{{ $item->product->product_name }}</label>
+                                </td>
+
+                                @if ($item->vendor_id == Null)
+                                <td class="col-md-2">
+                                    <label>Owner</label>
+                                </td>
+                                @else  
+                                <td class="col-md-2">
+                                    <label>{{ $item->product->vendor->name }}</label>
+                                </td>
+                                @endif
+                               
+
+                                <td class="col-md-2">
+                                    <label>{{ $item->product->product_code }}</label>
+                                </td>
+                                @if ($item->color == NULL)
+                                <td class="col-md-1">
+                                    <label>...</label>
+                                </td>
+                                @else
+                                <td class="col-md-1">
+                                    <label>{{ $item->product_color }}</label>
+                                </td>
+                                @endif
+                                
+                                @if ($item->size == NULL)
+                                <td class="col-md-1">
+                                    <label>...</label>
+                                </td>
+                                @else   
+                                <td class="col-md-1">
+                                    <label>{{ $item->product_size }}</label>
+                                </td>
+                                @endif
+                               
+                                <td class="col-md-1">
+                                    <label>{{ $item->qty }}</label>
+                                </td>
+
+                                <td class="col-md-3">
+                                    <label>${{ $item->price }} <br> Total = ${{ $item->price * $item->qty}} * </label>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
 @endsection
